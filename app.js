638ec6753e85bcedb12fc2cfa87edc2d673c4e88
Hoàng Phú <nhphu87@NHP-Mac.local>
2017-08-21 22:58:57 +0700

    First upload

---
+++ app.js
@@ -0,0 +1,225 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var formidable = require('formidable');
+var fs = require('fs');
+var execFile = require('child_process').execFile;
+var  bodyParser = require('body-parser');
+
+var resultPLFile = {
+    hasResult: false
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/')));
+var listFile = [];
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.get('/getListFile', function(req, res){
+  res.json(listFile);
+});
+
+app.post('/uploadToGetPointCloud', function(req, res, next){
+  // create an incoming form object
+  var form = new formidable.IncomingForm();
+  // specify that we want to allow the user to upload multiple files in a single request
+  form.multiples = true;
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '/uploads/getPointCloud');
+  // every time a file has been uploaded successfully,
+  // rename it to it's orignal name
+  form.on('file', function(field, file) {
+    fs.exists(path.join(form.uploadDir, file.name), (exists) =>{
+      if (!exists){
+        fs.appendFile('uploads/getPointCloud/dataset_files.txt', file.name + ' \n', (err) => {
+          if (err) next(err);
+          listFile.push(file.name);
+        });
+      }
+      fs.rename(file.path, path.join(form.uploadDir, file.name));
+    });
+  });
+  // log any errors that occur
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function() {
+    res.json(listFile);
+  });
+  // parse the incoming request containing the form data
+  form.parse(req);
+});
+var cornersImageName, findingBooksImageName;
+
+app.post('/uploadToGetCorners', function(req, res, next){
+  var form = new formidable.IncomingForm();
+
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '/uploads/getCorners');
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+    cornersImageName = file.name;
+  });
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+  form.on('end', function() {
+    res.json("upload successfully!");
+  });
+  // parse the incoming request containing the form data
+  form.parse(req);
+});
+
+app.post('/uploadToFindBooks', function(req, res, next){
+  function findingBooks(){
+    resultPLFile.hasResult = false;
+      execFile("C:\\Program Files\\DetectCorners\\FindingBooks.exe", ["uploads/findingBooks/" + findingBooksImageName, path.join(__dirname, "/output/books.jpg")], {maxBuffer: 1024 * 1024 * 64}, (error, stdout, stderr) => {
+      if (error) {
+         res.status(500).send({ error: error });
+      }else{
+        res.json('finding books successfully');
+      }
+      console.log(stdout);
+    });
+  }
+  var form = new formidable.IncomingForm();
+
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '/uploads/findingBooks');
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+    findingBooksImageName = file.name;
+    //findingBooks();
+  });
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  form.on('end', function() {
+    res.json("upload successfully!");
+  });
+  // parse the incoming request containing the form data
+  form.parse(req);
+});
+
+app.post('/uploadToGetHarrisDetector', function(req, res, next){ 
+  var form = new formidable.IncomingForm();
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '/uploads/HarrisDetector');
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+    HarrisDetectorImageName = file.name;
+    //findingBooks();
+  });
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  form.on('end', function() {
+    res.json("upload successfully!");
+  });
+  // parse the incoming request containing the form data
+  form.parse(req);
+});
+
+
+app.get('/removeFile', function(req, res, next) {
+  for(var i = 0; i<listFile.length; i++){
+    fs.unlink(path.join(__dirname, '/uploads/getPointCloud', listFile[i]));
+  }
+  listFile = [];
+  fs.exists(path.join(__dirname, 'output/pointcloud.txt'), (exists) =>{
+    if (exists){
+      fs.unlink(path.join(__dirname, 'output/pointcloud.txt' ));
+    }
+  });
+  resultPLFile.hasResult = false;
+  resultPLFile.err = false;
+  fs.writeFile('uploads/getPointCloud/dataset_files.txt', '', (err) => {
+    if (err){
+      res.status(500).send({ error: err });
+    }else {
+      res.sendFile(path.join(__dirname, 'views/index.html'));
+    }
+  });
+});
+
+app.get('/getPointcloudFile', function(req, res, next){
+  res.json(resultPLFile);
+});
+
+app.post('/getPointcloud', function(req, res, next){
+  function getPointcloud(){
+    resultPLFile.hasResult = false;
+    execFile("C:\\Program Files\\DetectCorners\\GetPointCloud.exe", ["uploads/getPointCloud/dataset_files.txt",req.body.focal, req.body.cx, req.body.cy], {maxBuffer: 1024 * 1024 * 64}, (error, stdout, stderr) => {
+      if (error) {
+         res.status(500).send({ error: error });
+         resultPLFile.err = true;
+         resultPLFile.errMes = error;
+      }else{
+        res.download('output/pointcloud.txt', 'output/pointcloud.txt', function(err){
+          if (err) {
+          } else {
+            // decrement a download credit, etc.
+          }
+        });
+      }
+      console.log(stdout);
+      resultPLFile.hasResult = true;
+    });
+  }
+  getPointcloud();
+});
+
+app.post('/getCorners', function(req, res, next){
+  function getCorners(){
+    execFile("C:\\Program Files\\DetectCorners\\DetectCorners.exe", ["uploads/getCorners/" + cornersImageName,req.body.maxCorners, req.body.quality_level, req.body.min_distance, path.join(__dirname, "/output/corners.jpg")], {maxBuffer: 1024 * 1024 * 64}, (error, stdout, stderr) => {
+      if (error) {
+         res.status(500).send({ error: error });
+      }else{
+        res.json('get corners successfully');
+      }
+      console.log(stdout);
+    });
+  }
+  getCorners();
+});
+
+app.post('/findingBooks', function(req, res, next){
+  function findingBooks(){
+      execFile("C:\\Program Files\\DetectCorners\\FindingBooks.exe", ["uploads/findingBooks/" + findingBooksImageName, req.body.threshold, path.join(__dirname, "/output/books.jpg")], {maxBuffer: 1024 * 1024 * 64}, (error, stdout, stderr) => {
+      if (error) {
+         res.status(500).send({ error: error });
+      }else{
+        res.json('finding books successfully');
+      }
+      console.log(stdout);
+    });
+  }
+  findingBooks();
+});
+
+app.post('/getHarrisDetector', function(req, res, next){
+  function getCorners(){
+      execFile("C:\\Program Files\\DetectCorners\\HarrisDetector.exe", ["uploads/HarrisDetector/" + HarrisDetectorImageName, req.body.threshold, path.join(__dirname, "/output/HarrisDetector.jpg")], {maxBuffer: 1024 * 1024 * 64}, (error, stdout, stderr) => {
+      if (error) {
+         res.status(500).send({ error: error });
+      }else{
+        res.json('HarrisDetector successfully');
+      }
+      console.log(stdout);
+    });
+  }
+  getCorners();
+});
+
+var server = app.listen(80, function(){
+  console.log('Server listening on port 80');
+});
---
+++ public/javascripts/controller.js
@@ -0,0 +1,241 @@
+var app = angular.module('myApp', ['ngMaterial']);
+
+app.directive('fileModel', ['$parse', function ($parse) {
+    return {
+       restrict: 'A',
+       link: function(scope, element, attrs) {
+          var model = $parse(attrs.fileModel);
+          var modelSetter = model.assign;
+          element.bind('change', function(){
+              var values = [];
+              angular.forEach(element[0].files, function (item) {
+                var value = item;
+                values.push(value);
+              });
+             scope.$apply(function(){
+                modelSetter(scope, values);
+             });
+          });
+       }
+    };
+ }]);
+
+app.service('fileUpload', ['$http', function ($http) {
+    this.getListFile = function(){
+      return $http.get('/getListFile');
+    }
+    this.uploadFileToUrl = function(files, uploadUrl){
+      var fd = new FormData();
+      for(var i = 0; i<files.length; ++i){
+        fd.append('file', files[i], files[i].name);
+      }
+      return $http.post(uploadUrl, fd, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+      });
+    }
+    this.removeFile = function(){
+      return $http.get('/removeFile');
+    }
+    this.getPL = function(pointcloud) {
+      return $http.post('/getPointcloud', pointcloud);
+    }
+    this.getPLF = function() {
+      return $http.get('/getPointcloudFile');
+    }
+ }]);
+
+app.service('cornersService',['$http', function($http) {
+ this.getCorners = function(corner) {
+   return $http.post('/getCorners', corner);
+ }
+}]);
+
+app.service('booksService',['$http', function($http) {
+ this.findBooks = function(threshold) {
+    //console.log(threshold);
+    return $http.post('/findingBooks', {threshold: threshold});
+ }
+}]);
+
+app.service('HarrisDetectorService',['$http', function($http) {
+ this.getCorners = function(threshold) {
+    //console.log(threshold);
+    return $http.post('/getHarrisDetector', {threshold: threshold});
+ }
+}]);
+
+
+app.controller('AppCtrl', ['$scope', 'fileUpload', '$timeout', 'cornersService', 'booksService', 'HarrisDetectorService', function($scope, fileUpload, $timeout, cornersService, booksService, HarrisDetectorService){
+  $scope.filesName = [];
+  $scope.dataLoadingSFM = false;
+  $scope.dataLoadingCorners = false;
+  $scope.dataLoadingBooks = false;
+  $scope.hasCorners = false;
+  $scope.hasBooks = false;
+  $scope.pointcloud = {
+    focal: null,
+    cx: null,
+    cy: null
+  };
+  fileUpload.getPLF()
+    .then(function(result){
+      if (result.data.hasResult){
+        if (!result.data.err){
+          $scope.showDownload = true;
+        }
+      }
+    }, function(err){
+  });
+
+  fileUpload.getListFile()
+    .then(function(result){
+      $scope.filesName = result.data;
+    }, function(err){
+      console.log(err);
+    });
+  $scope.uploadFile = function(){
+     var files = $scope.myFile;
+     var uploadUrl = "/uploadToGetPointCloud";
+     fileUpload.uploadFileToUrl(files, uploadUrl)
+     .then(function(result){
+       angular.forEach(files, function(item){
+         if ($scope.filesName.indexOf(item.name) == -1){
+            $scope.filesName.push(item.name);
+         }
+       });
+     }, function(err){
+       console.log(err);
+     });
+  };
+
+  $scope.renew = function(){
+      fileUpload.removeFile()
+        .then(function(result) {
+          console.log('ok remove');
+          $scope.filesName = [];
+          $scope.dataLoadingSFM = false;
+          $scope.pointcloud = {
+            focal: null,
+            cx: null,
+            cy: null
+          };
+          $scope.showDownload = false;
+        }, function(err){
+          console.log(err);
+        });
+  }
+  $scope.getPointCloud = function(pointcloud){
+      $scope.dataLoadingSFM = true;
+      console.log(pointcloud);
+      fileUpload.getPL(pointcloud)
+        .then(function(result){
+            console.log(result);
+            var anchor = angular.element('<a/>');
+            anchor.attr({
+                 href: 'data:attachment/txt;charset=utf-8,' + encodeURI(result.data),
+                 target: '_blank',
+                 download: 'pointcloud.txt'
+            })[0].click();
+            $scope.dataLoadingSFM = false;
+        }, function(err){
+            console.log(err);
+            function getPLFile(){
+              fileUpload.getPLF()
+                .then(function(result){
+                  if (result.data.hasResult){
+                    if (!result.data.err){
+                      var anchor = angular.element('<a/>');
+                      anchor.attr({
+                           href: '/pointcloud.txt',
+                           target: '_blank',
+                           download: 'pointcloud.txt'
+                      })[0].click();
+                      $scope.showDownload = true;
+                    }else{
+                      alert(result.data.errMes);
+                    }
+                    $scope.dataLoadingSFM = false;
+                  }else{
+                    $timeout(function(){
+                        getPLFile();
+                    }, 5000);
+                  }
+                }, function(err){
+                });
+            }
+          getPLFile();
+        });
+  }
+  $scope.corner = {
+    maxCorners : null,
+    quality_level : null,
+    min_distance : null
+  }
+   $scope.uploadToGetCorners = function(corners){
+      var files = $scope.myFile;
+      $scope.dataLoadingCorners = true;
+      var uploadUrl = "/uploadToGetCorners";
+      fileUpload.uploadFileToUrl(files, uploadUrl)
+      .then(function(result){
+        cornersService.getCorners(corners)
+         .then(function(){
+           $scope.hasCorners = true;
+          // $scope.outputCornersUrl = "";
+           $scope.outputCornersUrl = "output/corners.jpg?_ts=" + new Date().getTime();
+           $scope.dataLoadingCorners = false;
+         }, function(err) {
+           console.log(err);
+           $scope.dataLoadingCorners = false;
+         })
+      }, function(err){
+        console.log(err);
+        $scope.dataLoadingCorners = false;
+      });
+   };
+   $scope.uploadToFindBooks = function(){
+      var files = $scope.myFile;
+      $scope.dataLoadingBooks = true;
+      var uploadUrl = "/uploadToFindBooks";
+      fileUpload.uploadFileToUrl(files, uploadUrl)
+      .then(function(result){
+        console.log('update findBooks success');
+        booksService.findBooks($scope.threshold)
+          .then(function(){
+            $scope.hasBooks = true;
+        //   $scope.ouputBooksUrl = "";
+            $scope.outputBooksUrl = "output/books.jpg?_ts=" + new Date().getTime();
+            $scope.dataLoadingBooks = false;
+          }, function(error){
+            console.log(error);
+            $scope.dataLoadingBooks = false;
+          });
+      }, function(err){
+        console.log(err);
+        $scope.dataLoadingBooks = false;
+      });
+   };
+  $scope.uploadToGetHarrisDetector = function(){
+    var files = $scope.myFile;
+    $scope.dataLoadingHarrisCorner = true;
+    var uploadUrl = "/uploadToGetHarrisDetector";
+    fileUpload.uploadFileToUrl(files, uploadUrl)
+    .then(function(result){
+      console.log('update HarrisDetector success');
+      HarrisDetectorService.getCorners($scope.HarrisThreshold)
+        .then(function(){
+          $scope.hasHarrisDetector = true;
+      //   $scope.ouputBooksUrl = "";
+          $scope.outputHarrisDetectorUrl = "output/HarrisDetector.jpg?_ts=" + new Date().getTime();
+          $scope.dataLoadingHarrisCorner = false;
+        }, function(error){
+          console.log(error);
+          $scope.dataLoadingHarrisCorner = false;
+        });
+    }, function(err){
+      console.log(err);
+      $scope.dataLoadingHarrisCorner = false;
+    });
+  };
+
+}]);
